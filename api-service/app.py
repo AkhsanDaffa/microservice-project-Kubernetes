9b3216dff02508dc09c94e9373793d7753d8903b
AkhsanDaffa <akhsandaffa01@gmail.com>
2025-11-25 14:04:28 +0700

    Add Self-Healing Application

--- api-service/app.py
+++ api-service/app.py
@@ -3,6 +3,8 @@ import os
 
 app = Flask(__name__)
 
+is_healthy = True
+
 @app.route('/')
 def home():
     return "Hello, this is the API Service. Go to /api/data"
@@ -15,6 +17,19 @@ def get_data():
         data="Hello, ini adalah data rahasia dari API Service!",
         server_pod=pod_name
     )
+
+@app.route('/health')
+def health_check():
+    if is_healthy:
+        return "I am healthy", 200
+    else:
+        return "I am broken", 500
+
+@app.route('/sabotage')
+def sabotage():
+    global is_healthy
+    is_healthy = False
+    return "Service sabotaged! Health check will now fail.", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
